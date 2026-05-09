daa6b9f4030323b65a87d5c4f8937d4ef158c7e6
Duẩn <vanduanxxx@gmail.com>
2026-05-09 18:13:30 +0700

    Khử nhiễu

---
+++ huanluyen_khunhieu.py
@@ -0,0 +1,142 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from itertools import combinations
+from sklearn.metrics import mean_squared_error
+import joblib
+from scipy import stats
+
+df = pd.read_csv('airfoil_self_noise.dat', sep='\s+', header=None)
+df.columns = [
+    'Frequency', 
+    'Angle_of_Attack', 
+    'Chord_Length', 
+    'Stream_Velocity', 
+    'Displacement_Thickness', 
+    'Y'
+]
+df['Frequency'] = np.log10(df['Frequency'])
+
+X = df.drop(["Y"], axis=1) 
+y = df.get(["Y"]) 
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,    
+    random_state=45   
+)
+
+y_test = y_test.values 
+y_train = y_train.values 
+X_train = X_train.values
+X_test = X_test.values
+
+z_scores = np.abs(stats.zscore(X_train, axis=0))
+mask = (z_scores < 3).all(axis=1)
+X_train = X_train[mask]
+y_train = y_train[mask]
+
+scaler = StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+joblib.dump(scaler, 'scaler1.joblib')
+print("Đã lưu bộ chuẩn hóa lần 1 vào file scaler.joblib")
+
+def create_features(X):
+    n_samples, n_features = X.shape
+    one = np.ones((n_samples, 1))
+    interactions = [one]
+    interactions.append(X)
+    interactions.append(X**2)
+    interactions.append(X**3)
+    interactions.append(X**4)
+
+    for i, j in combinations(range(n_features), 2):
+        new_col = X[:, i] * X[:, j]
+        interactions.append(new_col)
+    for i, j in combinations(range(n_features), 2):
+        new_col = (X[:, i]**2) * X[:, j]
+        interactions.append(new_col)
+    for i, j in combinations(range(n_features), 2):
+        new_col = (X[:, i]) * (X[:, j]**2)
+        interactions.append(new_col)
+    for i, j in combinations(range(n_features), 2):
+        new_col = (X[:, i]**3) * X[:, j]
+        interactions.append(new_col)
+    for i, j in combinations(range(n_features), 2):
+        new_col = (X[:, i]) * (X[:, j]**3)
+        interactions.append(new_col)
+    for i, j in combinations(range(n_features), 2):
+        new_col = (X[:, i]**4) * X[:, j]
+        interactions.append(new_col)
+    for i, j in combinations(range(n_features), 2):
+        new_col = (X[:, i]) * (X[:, j]**4)
+        interactions.append(new_col)
+
+    return np.column_stack(interactions)
+     
+    
+def get_predict(x,w):
+    p=[]
+    for i in range(0,len(x)):
+        xi = x[i].reshape(-1,1)
+        y_pred= (w.T @ xi).item() 
+        p.append(y_pred)
+    return p
+
+X_train_poly = create_features(X_train)
+X_test_poly = create_features(X_test)
+
+scaler = StandardScaler().fit(X_train_poly[:, 1:])
+X_train_poly[:, 1:] = scaler.transform(X_train_poly[:, 1:])
+X_test_poly[:, 1:] = scaler.transform(X_test_poly[:, 1:])
+joblib.dump(scaler, 'scaler2.joblib')
+print("Đã lưu bộ chuẩn hóa lần 2 vào file scaler.joblib")
+
+n_samples, d = X_train_poly.shape
+w = np.zeros((d, 1))
+theta = 1e-4
+eta0 =0.00002
+
+k = 0
+graph = True
+
+plt.figure(figsize=(10,6))
+while True:
+    k += 1
+    eta=eta0*(1/(1 + 0.000001 * k))
+    w_old = w.copy()
+
+    for i in range(n_samples):
+        xi = X_train_poly[i].reshape(-1,1)
+        w = w+ eta * (y_train[i] - w.T @ xi).item() * xi
+    
+    if graph and k % 50 == 0:
+        y_pred = get_predict(X_train_poly,w)
+        print(f'Vòng lặp {k}: RMSE = {np.sqrt(mean_squared_error(y_train, y_pred)):.2f}')
+        plt.cla()
+        plt.plot(y_train[:100], color='blue', label='Actual')
+        plt.plot(y_pred[:100], color='red', label='Predicted')
+        plt.title(f'Iteration {k}')
+        plt.xlabel('Sample index')
+        plt.ylabel('Y')
+        plt.legend()
+        plt.pause(0.1)
+
+    if np.linalg.norm(w - w_old) < theta and k>5000:
+        break
+    if  k>100000:
+        break
+np.save('weights_model.npy', w)
+print("Đã lưu bộ trọng số vào file weights_model.npy")
+
+y_pred_final =get_predict(X_train_poly,w)
+plt.clf()
+plt.plot(y_train[:100], color='blue', label='Actual')
+plt.plot(y_pred_final[:100], color='red', label='Predicted')
+plt.title(f'Final model after {k} iterations')
+plt.xlabel('Sample index')
+plt.ylabel('Y')
+plt.legend()
+plt.show()
